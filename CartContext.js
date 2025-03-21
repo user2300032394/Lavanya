69f52cad843994b784dbf6df1bdeb911c42181dd
user2300032394 <2300032394@kluniversity.in>
2025-03-21 20:37:26 +0530

    Create CartContext.js

---
+++ CartContext.js
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === plant.id);
+      return existing
+        ? prev.map((item) =>
+            item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+          )
+        : [...prev, { ...plant, quantity: 1 }];
+    });
+  };
+
+  const increaseQuantity = (id) => {
+    setCart((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
+    );
+  };
+
+  const decreaseQuantity = (id) => {
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
+      )
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, increaseQuantity, decreaseQuantity, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
